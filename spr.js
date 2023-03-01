a99dd47ce49a3743591c6b0a1aa1b47ce2d87d3a
CROpie <96667010+CROpie@users.noreply.github.com>
2023-03-01 22:18:30 +1100

    Add a functional frontend

--- spr.js
+++ spr.js
@@ -6,28 +6,16 @@ function getComputerChoice() {
     return getRandomInt(3);
 }
 
-function getPlayerChoice() {
-    hand_throw = prompt("S/P/R");
-    hand_throw = hand_throw.toLowerCase();
-    if (hand_throw === "s") {
-        return 0;
-    } else if (hand_throw === "p") {
-        return 1;
-    } else {
-        return 2;
-    }
-}
-
 function determineWinner(computer, player) {
     if (computer === player) {
-        console.log("Draw!");
-        return "D";
+        results_table.textContent = 'Draw!'
+        updateScore("D");
     }   else if ((player === 0 && computer === 1) || (player === 1 && computer === 2) || (player === 2 && computer === 0)) {
-        console.log("You win!");
-        return "P";
+        results_table.textContent = 'You Win!'
+        updateScore("P");
     }   else {
-        console.log("You lose!");
-        return "C";
+        results_table.textContent = 'You Lose!'
+        updateScore("C");
     } 
 }
 
@@ -37,19 +25,68 @@ function playRound() {
     return determineWinner(computer, player);
 }
 
-function game() {
-    player_score = 0;
-    computer_score = 0;
-    for (let i = 0; i < 5; i++) {
-        winner = playRound()
-        if (winner === "P") {
-            player_score++
-        } else if (winner === "C") {
-            computer_score++
-        }
+function updateScore(round_outcome) {
+    if (round_outcome === "P") {
+        player_score++
+        table_wins.textContent = player_score;
+    } else if (round_outcome === "C") {
+        computer_score++
+        table_losses.textContent = computer_score;
+    } else {
+        draws_score++
+        table_draws.textContent = draws_score;
     }
-    console.log(player_score);
-    console.log(computer_score);
+    check_for_winner()
 }
 
-game()
+function check_for_winner() {
+    if (player_score === 5) {
+        results_table.textContent = 'You Have Beaten The Computer!!'
+    } else if (computer_score === 5) {
+        results_table.textContent = 'You Lost To The Computer!!'
+    }
+}
+
+function reset_scores() {
+    table_wins.textContent = (player_score = 0);
+    table_losses.textContent = (computer_score = 0);
+    table_draws.textContent = (draws_score = 0);
+}
+
+let player_score = 0;
+let computer_score = 0;
+let draws_score = 0;
+
+function playGame(throw_choice) {
+    computer = getComputerChoice();
+    return determineWinner(computer, throw_choice);
+}
+const play_scissors = document.querySelector('#scissors');
+const play_paper = document.querySelector('#paper');
+const play_rock = document.querySelector('#rock');
+const results_table = document.querySelector('.results');
+const reset_button = document.querySelector('#reset');
+
+const table_wins = document.querySelector('#player_wins');
+const table_losses = document.querySelector('#computer_wins');
+const table_draws = document.querySelector('#nobody_wins');
+
+table_wins.textContent = player_score;
+table_losses.textContent = computer_score;
+table_draws.textContent = draws_score;
+
+play_scissors.addEventListener('click', () => {
+    playGame(0);
+});
+
+play_paper.addEventListener('click', () => {
+    playGame(1);
+});
+
+play_rock.addEventListener('click', () => {
+    playGame(2);
+});
+
+reset_button.addEventListener('click', () => {
+    reset_scores();
+});
