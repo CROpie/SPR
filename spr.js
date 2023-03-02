68ffe40efc15b38b575f9ea97af4bd6d30f1ef09
CROpie <96667010+CROpie@users.noreply.github.com>
2023-03-02 14:15:10 +1100

    Introduce .forEach, .onlick and .dataset.num to clean up the code

--- spr.js
+++ spr.js
@@ -1,3 +1,42 @@
+function unfreeze_buttons() {
+    document.querySelectorAll('.throw').forEach(choice => {
+        choice.disabled = false;
+    });
+}
+
+function freeze_buttons() {
+    document.querySelectorAll('.throw').forEach(choice => {
+            choice.disabled = true;
+    });
+}
+
+function check_for_winner() {
+    if (player_score === 5) {
+        results_table.textContent = 'You Have Beaten The Computer!!'
+        freeze_buttons()
+    } else if (computer_score === 5) {
+        results_table.textContent = 'You Lost To The Computer!!'
+        freeze_buttons()
+    }
+}
+
+function determineWinner(computer, player) {
+    if (computer === player) {
+        draws_score++
+        results_table.textContent = 'Draw!'
+        table_draws.textContent = draws_score;
+    }   else if ((player === 0 && computer === 1) || (player === 1 && computer === 2) || (player === 2 && computer === 0)) {
+        player_score++
+        results_table.textContent = 'You Win!'
+        table_wins.textContent = player_score;
+    }   else {
+        computer_score++
+        results_table.textContent = 'You Lose!'
+        table_losses.textContent = computer_score;
+    } 
+    check_for_winner()
+}
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -6,87 +45,36 @@ function getComputerChoice() {
     return getRandomInt(3);
 }
 
-function determineWinner(computer, player) {
-    if (computer === player) {
-        results_table.textContent = 'Draw!'
-        updateScore("D");
-    }   else if ((player === 0 && computer === 1) || (player === 1 && computer === 2) || (player === 2 && computer === 0)) {
-        results_table.textContent = 'You Win!'
-        updateScore("P");
-    }   else {
-        results_table.textContent = 'You Lose!'
-        updateScore("C");
-    } 
-}
-
-function playRound() {
-    computer = getComputerChoice();
-    player = getPlayerChoice();
-    return determineWinner(computer, player);
-}
-
-function updateScore(round_outcome) {
-    if (round_outcome === "P") {
-        player_score++
-        table_wins.textContent = player_score;
-    } else if (round_outcome === "C") {
-        computer_score++
-        table_losses.textContent = computer_score;
-    } else {
-        draws_score++
-        table_draws.textContent = draws_score;
-    }
-    check_for_winner()
-}
-
-function check_for_winner() {
-    if (player_score === 5) {
-        results_table.textContent = 'You Have Beaten The Computer!!'
-    } else if (computer_score === 5) {
-        results_table.textContent = 'You Lost To The Computer!!'
-    }
-}
-
-function reset_scores() {
-    table_wins.textContent = (player_score = 0);
-    table_losses.textContent = (computer_score = 0);
-    table_draws.textContent = (draws_score = 0);
-}
-
-let player_score = 0;
-let computer_score = 0;
-let draws_score = 0;
-
 function playGame(throw_choice) {
     computer = getComputerChoice();
     return determineWinner(computer, throw_choice);
 }
-const play_scissors = document.querySelector('#scissors');
-const play_paper = document.querySelector('#paper');
-const play_rock = document.querySelector('#rock');
-const results_table = document.querySelector('.results');
-const reset_button = document.querySelector('#reset');
 
+// reset both the score text fields, as well as the associated constants
+function reset_scores() {
+    table_wins.textContent = (player_score = 0);
+    table_losses.textContent = (computer_score = 0);
+    table_draws.textContent = (draws_score = 0);
+    unfreeze_buttons();
+}
+
+// Initialize score variables
+let player_score = 0;
+let computer_score = 0;
+let draws_score = 0;
+
+// Set up variables to easly refer to the text fields
+const results_table = document.querySelector('#results');
 const table_wins = document.querySelector('#player_wins');
 const table_losses = document.querySelector('#computer_wins');
 const table_draws = document.querySelector('#nobody_wins');
 
-table_wins.textContent = player_score;
-table_losses.textContent = computer_score;
-table_draws.textContent = draws_score;
+// Assigning the reset score function to the reset button
+document.querySelector('#reset').onclick = reset_scores;
 
-play_scissors.addEventListener('click', () => {
-    playGame(0);
-});
-
-play_paper.addEventListener('click', () => {
-    playGame(1);
-});
-
-play_rock.addEventListener('click', () => {
-    playGame(2);
-});
-
-reset_button.addEventListener('click', () => {
-    reset_scores();
+// Make the buttons functional
+document.querySelectorAll('.throw').forEach(choice => {
+    choice.onclick = () => {
+        playGame(parseInt(choice.dataset.num))
+    }
 });
